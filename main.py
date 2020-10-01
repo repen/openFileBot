dc7fa725be35a15a1672d7a6616980ce48690f06
repen <9keepa@gmail.com>
2020-10-01 11:39:07 +0500

    default value when not data

--- main.py
+++ main.py
@@ -18,20 +18,25 @@ def script_files():
 
     files = listdir_fullpath(PATH_DIR)
     log.info("Load files:\n%s", "\n".join(files))
-    if not files:
-        log.info("No files")
-        return -1
+
 
     files = sorted(files, key= lambda x :  os.path.basename(x)  )
     files = list( filter( lambda x : re.search(r"\.txt", x), files) )
     log.info("Sorted and Filter files:\n%s", "\n".join(files))
+
+    if not files:
+        log.info("No files")
+        return -1
 
     values = []
     for file in files:
         with open(file) as f:
             data = f.read()
         data = data.strip()
-        values.append( data )
+        if not data:
+            values.append( "0;0;0;0;0" )
+        else:
+            values.append( data )
     string = ";".join(values)
     log.info("Join values: %s", string)
     with open(OUTPUT, "w") as f:
